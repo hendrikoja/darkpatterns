7c87a432a5d4a588c3418d4741551a961e7ff9d2
andruspeegel <andrusp@tlu.ee>
2022-06-14 11:54:07 +0300

    manualmerge

--- resources/js/components/Story.vue
+++ resources/js/components/Story.vue
@@ -39,7 +39,6 @@ export default{
       <img src="../../static/boss2.png" class="storybosspng">
       <img src="../../static/boss2.png" class="storybosspng2">
        <img src="../../static/epicworkerdoingtheworkingfortheboss.png" class="storyworkerpng">
-      
     </div>
 </div> 
 </template>
