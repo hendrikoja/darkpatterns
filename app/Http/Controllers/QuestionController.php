1c084f6669024f0bc8026880caef7a06dd1c52d1
hendrikoja <90192364+hendrikoja@users.noreply.github.com>
2022-06-14 15:23:23 +0300

    Küsimuste lisamine PHP stuff

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -13,4 +13,40 @@ class QuestionController extends Controller
         $questions = Question::with("answers", "boss_answers", "question_story") -> get();
         return response() -> json($questions);
     }
+
+    public function add_question(Request $request) {
+        $question = new Question();
+        $question->description = $request->description;
+        $question->points = $request->points; 
+        $question->save();
+    }
+
+    //TODO: Piltide salvestamine, selle järgi pildi asukoha võtmine.
+    public function add_answer(Request $request) {
+        $question = Question::find($request->question_id);
+
+        $answer = new AnswerToQuestion();
+        $answer->description = $request->description;
+        $anser->correct = $request->correct;
+
+        $question->answers()->save($answer);
+    }
+
+    public function add_story(Request $request) {
+        $question = Question::find($request->question_id);
+        
+        $story = new BossStory();
+        $story->description = $request->description;
+
+        $question->question_story()->save($story);
+    }
+
+    public function add_boss_answer(Request $request) {
+        $question = Question::find($request->question_id);
+        
+        $story = new BossAnswer();
+        $story->description = $request->description;
+
+        $question->boss_answers()->save($story);
+    }
 }
