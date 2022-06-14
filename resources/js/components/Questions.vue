9a3f2c05c6c064050f82d7cf9a868a239d84932d
KristoAaslaid <90237392+KristoAaslaid@users.noreply.github.com>
2022-06-14 10:43:25 +0300

    Update Questions.vue

--- resources/js/components/Questions.vue
+++ resources/js/components/Questions.vue
@@ -1,23 +1,8 @@
-<script>
-export default{
-    data(){
-        return {
-            count: 0
-        }
-    },
-    mixins: [
-        require('./MethodsVue.vue')
-    ]
-}
-// Script on vaja muuta, temp. lahendus praegu
-</script>
-
 <template>
 <div class="question">
   <img src="../../static/textbubble.png" class="QuestionsTextbubble">
   <img src="../../static/fakedarkpattern.png" class="QuestionsChoice1">
   <img src="../../static/cookieyum.png" class="QuestionsChoice2">
   <button name="Answer" class="answerbutton" @click="$emit('questionevent')">Answer</button>
-  <button name="openscreen" class="button1" @click="funnistuff()">{{count}}</button>
 </div> 
 </template>
