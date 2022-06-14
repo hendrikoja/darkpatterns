9bf04891b36ff44532b891e988d8b3d9bc8fbdd0
Christopher Tokko <ctokko@tlu.ee>
2022-06-14 10:41:14 +0300

    le typewriter part 2 page 2
    typewriter go brrr cookies...yum

--- resources/js/components/Questions.vue
+++ resources/js/components/Questions.vue
@@ -16,9 +16,15 @@ export default{
 
 <template>
 <div class="question">
-  <img src="../../static/textbubble.png" class="QuestionsTextbubble">
-  <img src="../../static/fakedarkpattern.png" class="QuestionsChoice1">
-  <img src="../../static/cookieyum.png" class="QuestionsChoice2">
+    <img src="../../static/textbubble.png" class="QuestionsTextbubble">
+    <img src="../../static/fakedarkpattern.png" class="QuestionsChoice1">
+    <img src="../../static/cookieyum.png" class="QuestionsChoice2">
+    <div class="bossitekst4">
+      <p class="line-1 anim-typewriter4">One of the following images is a dark pattern while the other one is not.</p>
+    </div>
+    <div class="bossitekst5">
+      <p class="line-1 anim-typewriter5">Choose the dark pattern....be careful!</p>
+    </div>
   <button name="Answer" class="answerbutton" @click="$emit('questionevent')">Answer</button>
 </div> 
 </template>
