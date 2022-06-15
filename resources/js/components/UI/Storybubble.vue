3d47f42555b2d0ba4901163fcaab4019f6ac64de
Hendrik Oja <90192364+hendrikoja@users.noreply.github.com>
2022-06-15 17:47:55 +0300

    Esimesed bootleg animationid

--- resources/js/components/UI/Storybubble.vue
+++ resources/js/components/UI/Storybubble.vue
@@ -6,8 +6,11 @@ export default{
         }
     },
     mounted() {
+       setTimeout(() => this.hidden = false, 500 + (1000 * this.order_number));
+       console.log(this.order_number);
     },
     methods: {
+
     },
 
     props: [
@@ -18,12 +21,34 @@ export default{
 </script>
 
 <template>
-<div class="storybubbles" v-if="hidden">
-    <div class="storybosspics">
-      <img src="../../../static/boss2.png" class="storybosspng">
-    </div>
-    <div class="bossitekst">
-      <p>{{order_number}} : {{prompt}}</p>
-    </div>
-</div> 
+  <Transition name="bounce">
+  <div class="storybubbles" v-if="!hidden">
+      <div class="storybosspics">
+        <img src="../../../static/boss2.png" class="storybosspng">
+      </div>
+      <div class="bossitekst">
+        <p>{{prompt}}</p>
+      </div>
+  </div> 
+  </Transition>
 </template>
+
+<style>
+  .bounce-enter-active {
+    animation: bounce-in 0.5s;
+  }
+  .bounce-leave-active {
+    animation: bounce-in 0.5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.25);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+</style>
