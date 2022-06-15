1a72c0cd8c1b7e8522b92aed173322c0c5ead76a
Christopher Tokko <ctokko@tlu.ee>
2022-06-15 10:50:02 +0300

    leaderboard
    based leaderboard

--- resources/js/components/Leaderboard.vue
+++ resources/js/components/Leaderboard.vue
@@ -3,7 +3,39 @@
 </script>
 
 <template>
-<div class="Leaderboard">
-    <p>Leaderboard or smth, cum</p>
+<div class="Leaderboard2">
+    <img class="scale" src="../../static/interactivescreenbig.png">
+	<div class="container">
+		<div class="leaderboard">
+			<div class="head">
+				<i class="fas fa-crown"></i>
+				<h1>Top Scores</h1>
+			</div>
+			<div class="body">
+				<ol>
+					<li>
+						<mark>Andrus Ansip</mark>
+						<small>948</small>
+					</li>
+					<li>
+						<mark>Peeter Parkija</mark>
+						<small>750</small>
+					</li>
+					<li>
+						<mark>Mari Maasikas</mark>
+						<small>684</small>
+					</li>
+					<li>
+						<mark>Ämber Höörd</mark>
+						<small>335</small>
+					</li>
+					<li>
+						<mark>Egon Ubina</mark>
+						<small>-1000</small>
+					</li>
+				</ol>
+			</div>
+		</div>
+	</div>
 </div>
 </template>
