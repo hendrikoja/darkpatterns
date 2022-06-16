62b88ab0c0d777e6ca2e73b2c21e5971638d502a
hendrikoja <90192364+hendrikoja@users.noreply.github.com>
2022-06-16 10:06:54 +0300

    Küsimuste vastamise optimization, veits broken

--- resources/js/components/Questions.vue
+++ resources/js/components/Questions.vue
@@ -1,5 +1,6 @@
 <script setup>
   import Questionbubble from './UI/Questionbubble.vue'
+  import Answer from './UI/Answer.vue'
 </script>
 <script>
 export default{
@@ -18,16 +19,27 @@ export default{
 </script>
 
 <template>
-<div class="question center">
+<div class="questionPage">
     <Questionbubble
       :prompt="this.question_data['description']"
     />
-    <div class="QuestionsChoice1">
-      <img src="../../static/acceptcookies1.png" class="scale" @click="answeredfunc()">
-    </div>
-    <div class="QuestionsChoice2">
-      <img src="../../static/acceptcookies2.png" class="scale" @click="answeredfunc()">
+    <div class="questionContainer">
+      <Answer @click="$emit('questionevent')" />
+      <Answer @click="$emit('questionevent')" />
     </div>
   <button v-if="answered" name="Answer" class="answerbutton" @click="$emit('questionevent')">Answer</button>
 </div> 
 </template>
+
+<style scoped>
+  .questionContainer {
+    display:flex;
+    position: absolute;
+    bottom: 1vh;
+    justify-content: center;
+    align-items: flex-end;
+    height: 15vh;
+    width: 100vw;
+    gap: 1vw;
+  }
+</style>
---
+++ resources/js/components/UI/Answer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="QuestionsChoice1">
+        <img src="../../../static/acceptcookies1.png" class="scale" @click="answeredfunc()">
+    </div>
+</template>
+
+<style scoped>
+.QuestionsChoice1 {
+  max-width: 25vw;
+  height: auto;
+}
+</style>
