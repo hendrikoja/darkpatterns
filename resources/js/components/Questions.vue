08d25fdfd85ff28c06e6831813e6cce32ecf071e
KristoAaslaid <90237392+KristoAaslaid@users.noreply.github.com>
2022-06-16 10:15:09 +0300

    Küsimuste valikutel shadow peale klikkimist
    Loogikat vaja täiendada tho.

--- resources/js/components/Questions.vue
+++ resources/js/components/Questions.vue
@@ -22,11 +22,11 @@ export default{
     <Questionbubble
       :prompt="this.question_data['description']"
     />
-    <div class="QuestionsChoice1">
-      <img src="../../static/acceptcookies1.png" class="scale" @click="answeredfunc()">
+    <div class="QuestionsChoice1" @click="answeredfunc()" tabindex="1">
+      <img src="../../static/acceptcookies1.png" class="scale">
     </div>
-    <div class="QuestionsChoice2">
-      <img src="../../static/acceptcookies2.png" class="scale" @click="answeredfunc()">
+    <div class="QuestionsChoice2" @click="answeredfunc()" tabindex="1">
+      <img src="../../static/acceptcookies2.png" class="scale">
     </div>
   <button v-if="answered" name="Answer" class="answerbutton" @click="$emit('questionevent')">Answer</button>
 </div> 
