bbb3055182c3d53162555d1cbb9919b92b80e120
KristoAaslaid <90237392+KristoAaslaid@users.noreply.github.com>
2022-06-16 10:25:42 +0300

    Questioni stiil update
    Questioni stiil update

--- resources/js/components/UI/Answer.vue
+++ resources/js/components/UI/Answer.vue
@@ -1,6 +1,15 @@
+<script>
+export default{
+  mixins: [
+    require('../MethodsVue.vue')
+  ]
+}
+</script>
+
+
 <template>
     <div class="QuestionsChoice1">
-        <img src="../../../static/acceptcookies1.png" class="scale" @click="answeredfunc()">
+        <img src="../../../static/acceptcookies1.png" class="scale" @click="answeredfunc()" tabindex="1">
     </div>
 </template>
 
@@ -8,5 +17,11 @@
 .QuestionsChoice1 {
   max-width: 25vw;
   height: auto;
+  background: #2196F3;
+  border: 2px solid #222;
+}
+
+.QuestionsChoice1:focus{
+  box-shadow: 0 0 2px 2px #000;
 }
 </style>
