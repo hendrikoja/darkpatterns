8ff403bdc4a05cd9bf323f2b907d17825144c40d
KristoAaslaid <90237392+KristoAaslaid@users.noreply.github.com>
2022-06-17 13:22:12 +0300

    Localisation

--- resources/js/components/Openingscreen.vue
+++ resources/js/components/Openingscreen.vue
@@ -23,7 +23,7 @@ export default{
   <div class="center">
     <button name="openscreen" class="button1" @click="countup()">{{count}}</button>
     <input class="user" v-model="username" placeholder="Username" maxlength="14" @change="emitUsernameChange()">
-    <button class="enteruser" @click="$emit('openingevent')">Start game lol</button>
+    <button class="enteruser" @click="$emit('openingevent')">Start game</button>
     <div class="mainmenutextbubble">
         <h2 class="gameTitle">Dark Patterns</h2><br><p class="startScreenIntroText">A dark pattern is a user interface that has been carefully crafted to trick users into doing things, such as buying overpriced insurance with their purchase or signing up for recurring bills.</p>
         <h2 class="gameTitle">The Game</h2><br><p class="startScreenIntroText">In the following game, you'll assume the role of an UX designer in a Fortune 500 company, where you're attempting to bring the company as much profit as possible by implementing dark patterns into your company's practices. You'll get points for making decisions which are good for the company, but bad for the user.</p>
